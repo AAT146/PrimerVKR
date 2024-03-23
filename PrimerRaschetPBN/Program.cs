3a48d7cb10e1d65b61e5c808a2e0b1b94e66a7c8
AAT146 <tupikova.a.00@yandex.ru>
2024-03-23 18:29:19 +0700

    fix(Program): не изменяет параметр в файле .rst

--- PrimerRaschetPBN/Program.cs
+++ PrimerRaschetPBN/Program.cs
@@ -16,44 +16,48 @@ namespace PrimerRaschetPBN
             Rastr rastr = new Rastr();
 
             // Загрузка файла с данными
-            string patch = @"C:\Users\aat146\Desktop\ПримерКП\Режим1.rst";
-            rastr.Load(RG_KOD.RG_REPL, patch, " ");
+            string file = @"C:\Users\aat146\Desktop\ПримерКП\Режим1.rst";
+
+            // string shablon = @"C:\Users\aat146\Documents\RastrWin3\SHABLON\динамика.rst";
+            rastr.Load(RG_KOD.RG_REPL, file, "");
 
             // Объявление переменной, тип - таблицаю
             var tables = rastr.Tables;
 
             // Объявление объекта, содержащего таблицу "Узлы"
-            var node = tables.Item("Node");
+            var node = tables.Item("node");
 
             // Создание объектов, содержащих информацию по каждой колонке
-            var numberNode = node.Cols.Item("ny"); // Номер узла
-            var nameNode = node.Cols.Item("name"); // Название узла
-            var numberArea = node.Cols.Item("na"); // Номер района
-            var powerActiveLoad = node.Cols.Item("pn"); // Активная мощность нагрузки.
-            var powerRectiveLoad = node.Cols.Item("qn"); // Реактивная мощность нагрузки.
-            var powerActiveGeneration = node.Cols.Item("pg"); // Активная мощность генерации.
-            var powerRectiveGeneration = node.Cols.Item("qg"); // Реактивная мощность генерации.
-            var voltageCalc = node.Cols.Item("vras"); // Расчётное напряжение.
-            var deltaCalc = node.Cols.Item("delta"); // Расчётный угол.
+            var numberNode = node.Cols.Item("ny");   // Номер узла
+            var nameNode = node.Cols.Item("name");   // Название узла
+            var numberArea = node.Cols.Item("na");   // Номер района
+            var powerActiveLoad = node.Cols.Item("pn");   // Активная мощность нагрузки.
+            var powerRectiveLoad = node.Cols.Item("qn");   // Реактивная мощность нагрузки.
+            var powerActiveGeneration = node.Cols.Item("pg");   // Активная мощность генерации.
+            var powerRectiveGeneration = node.Cols.Item("qg");   // Реактивная мощность генерации.
+            var voltageCalc = node.Cols.Item("vras");   // Расчётное напряжение.
+            var deltaCalc = node.Cols.Item("delta");   // Расчётный угол.
 
             // Объявление объекта, содержащего таблицу "Ветви"
-            var vetv = tables.Item("Vetv");
+            var vetv = tables.Item("vetv");
 
             // Создание объектов, содержащих информацию по каждой колонке
-            var staVetv = vetv.Cols.Item("sta"); // Состояние ветви.
+            var staVetv = vetv.Cols.Item("sta");   // Состояние ветви.
 
-            // Изменение Рг в строку 2, таблицы Узлы
-            var setSelVoltage = "ny=" + 10;
-            node.SetSel(setSelVoltage);
-            var nodeNumber = node.FindNextSel(-1);
-            var u10 = voltageCalc.Z[nodeNumber];
-            Console.WriteLine($"Напряжение в узле 10 равно: {u10} кВ.");
+            // Вывод в консоль название узла
+            var setSelName = "ny=" + 10;   // Переменная ny = 10 (№ узла = 10)
+            node.SetSel(setSelName);   // Выборка по переменной.
+            var index = node.FindNextSel(-1);   // Возврат индекса след.строки, удовл-ей выборке (искл: -1).
+            var name10 = nameNode.Z[index];   // Переменная с найденным индексом в столбце Название.
+            int p = 550;
+            powerActiveGeneration.Z[index] = p;
+            Console.WriteLine($"Название узла 10: {name10}.");
 
             // Расчет УР
-            _ = rastr.rgm(" ");
+            rastr.rgm("");
 
-            string patchNew = @"C:\Users\aat146\Desktop\ПримерКП\Режим2.rst";
-            rastr.Save(patchNew, " ");
+            string fileNew = @"C:\Users\aat146\Desktop\ПримерКП\Режим2.rst";
+            rastr.Save(fileNew, "");
         }
     }
 }
