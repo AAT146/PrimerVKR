5eb589fbc5e98d8c8e089fe53003f737d38516a3
AAT146 <tupikova.a.00@yandex.ru>
2024-03-26 15:07:36 +0700

    Нужно разобраться с циклом, контролируемыми величинами и м. Монте-Карло

--- PrimerRaschetPBN/Program.cs
+++ PrimerRaschetPBN/Program.cs
@@ -17,9 +17,10 @@ namespace PrimerRaschetPBN
 
             // Загрузка файла с данными
             string file = @"C:\Users\aat146\Desktop\ПримерКП\Режим1.rst";
+            string shablon = @"C:\Programs\RastrWin3\RastrWin3\SHABLON\динамика.rst";
 
             // string shablon = @"C:\Users\aat146\Documents\RastrWin3\SHABLON\динамика.rst";
-            rastr.Load(RG_KOD.RG_REPL, file, "");
+            rastr.Load(RG_KOD.RG_REPL, file, shablon);
 
             // Объявление переменной, тип - таблицаю
             var tables = rastr.Tables;
@@ -27,16 +28,19 @@ namespace PrimerRaschetPBN
             // Объявление объекта, содержащего таблицу "Узлы"
             var node = tables.Item("node");
 
+            // Объявление объекта, содержащего таблицу "Generator"
+            var generator = tables.Item("Generator");
+
             // Создание объектов, содержащих информацию по каждой колонке
             var numberNode = node.Cols.Item("ny");   // Номер узла
             var nameNode = node.Cols.Item("name");   // Название узла
             var numberArea = node.Cols.Item("na");   // Номер района
             var powerActiveLoad = node.Cols.Item("pn");   // Активная мощность нагрузки.
-            var powerRectiveLoad = node.Cols.Item("qn");   // Реактивная мощность нагрузки.
-            var powerActiveGeneration = node.Cols.Item("pg");   // Активная мощность генерации.
             var powerRectiveGeneration = node.Cols.Item("qg");   // Реактивная мощность генерации.
             var voltageCalc = node.Cols.Item("vras");   // Расчётное напряжение.
             var deltaCalc = node.Cols.Item("delta");   // Расчётный угол.
+
+            var powerActiveGeneration = generator.Cols.Item("P");   // Активная мощность генерации.
 
             // Объявление объекта, содержащего таблицу "Ветви"
             var vetv = tables.Item("vetv");
@@ -49,15 +53,15 @@ namespace PrimerRaschetPBN
             node.SetSel(setSelName);   // Выборка по переменной.
             var index = node.FindNextSel(-1);   // Возврат индекса след.строки, удовл-ей выборке (искл: -1).
             var name10 = nameNode.Z[index];   // Переменная с найденным индексом в столбце Название.
-            int p = 550;
+            int p = 500;
             powerActiveGeneration.Z[index] = p;
             Console.WriteLine($"Название узла 10: {name10}.");
 
             // Расчет УР
-            rastr.rgm("");
+            _ = rastr.rgm("");
 
             string fileNew = @"C:\Users\aat146\Desktop\ПримерКП\Режим2.rst";
-            rastr.Save(fileNew, "");
+            rastr.Save(fileNew, shablon);
         }
     }
 }
